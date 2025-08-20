d651abaddc3779b8cb09e7b26e0fdc93b237648a
Retryqwerty <88586098+Retryqwerty@users.noreply.github.com>
2025-08-20 15:27:26 +0300

    Delete backend directory

--- backend/run_backend.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-if [ ! -d ".venv" ]; then
-  python3 -m venv .venv
-fi
-source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-exec uvicorn main:app --host 0.0.0.0 --port 8000
